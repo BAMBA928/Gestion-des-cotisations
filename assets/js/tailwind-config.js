35ad2bb332190842d095b7027f721e0f4ee9ecc6
BAMBA928 <bambapartou42@gmail.com>
2026-07-12 16:38:33 +0000

    fix: correction des liens et navigation

---
+++ assets/js/tailwind-config.js
@@ -0,0 +1,26 @@
+  tailwind.config = {
+    theme: {
+      extend: {
+        fontFamily: {
+          display: ['Space Grotesk', 'sans-serif'],
+          sans: ['Inter', 'sans-serif'],
+        },
+        colors: {
+
+          gold: {
+            400: '#e0b559',
+            500: '#d4a857',
+          },
+          brand: {
+            600: '#2f5fd6',
+            700: '#274ec2',
+          },
+          leaf: {
+            600: '#1f9d5a',
+            700: '#17824a',
+          },
+          
+        },
+      },
+    },
+  }
